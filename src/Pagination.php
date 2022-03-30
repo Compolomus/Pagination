2c03d3cf46d2b54620b5ff17dd0d6865d735cc96
Compolomus <compolom@gmail.com>
2022-03-30 17:35:36 +0200

    Add tests

--- src/Pagination.php
+++ src/Pagination.php
@@ -1,30 +1,33 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Compolomus\Pagination;
 
 class Pagination
 {
-    private $total;
+    private int $total;
 
-    private $page;
+    private int $page;
 
-    private $limit;
+    private int $limit;
 
-    private $totalPages;
+    private int $totalPages;
 
-    private $length;
-    
-    private $pos;
+    private int $length;
+
+    private string $pos;
+
+    private bool $uiKeys;
 
     /**
-     * Pagination constructor.
-     *
      * @param int $page
      * @param int $limit
      * @param int $total
      * @param int $length
+     * @param bool $uiKeys
      */
-    public function __construct(int $page, int $limit, int $total, int $length = 3, bool $uiKeys = true)
+    public function __construct(int $page, int $limit, int $total, int $length = 3, bool $uiKeys = false)
     {
         $this->totalPages = (int)ceil($total / $limit);
         $this->page = $page > 1 ? ($page > $this->totalPages ? 1 : $page) : 1;
@@ -34,27 +37,27 @@ class Pagination
         $this->pos = $this->init();
         $this->uiKeys = $uiKeys;
     }
-    
+
     private function init(): string
     {
-		$pos = 'full';
-		
-		switch ($this->page) {
-			case (($this->page - $this->length) < 3):
-				$pos  = 'noLeftDots';
-				break;
-			case (($this->page - $this->length) >= 3 && ($this->totalPages - $this->page - $this->length) > 1):
-				$pos  = 'center';
-				break;
-			case (abs($this->totalPages - $this->page - $this->length) >= 0):
-				$pos  = 'noRightDots';
-				break;
-			case ($this->totalPages < 7):
-				$pos = 'full';
-				break;
-		}
-		
-		return $pos;
+        switch ($this->page) {
+            case ($this->totalPages < 7):
+            default:
+                $pos = 'full';
+                break;
+            case (($this->page - $this->length) < 3):
+                $pos  = 'noLeftDots';
+                break;
+            case (($this->page - $this->length) >= 3 && ($this->totalPages - $this->page - $this->length) > 1):
+                $pos  = 'center';
+                break;
+            case (abs($this->totalPages - $this->page - $this->length) >= 0):
+                $pos  = 'noRightDots';
+                break;
+        }
+
+        return $pos;
+
     }
 
     /**
@@ -91,19 +94,18 @@ class Pagination
         if ($this->page > 1) {
             $result['prev'] = $this->page - 1;
         }
-        
+
         if ($this->page !== 1) {
             $result['first'] = 1;
         }
-        
+
         if ($this->page > 3) {
-			$result['second'] = 2;
-		}
+            $result['second'] = 2;
+        }
 
         return $this->uiKeys ? $result : [];
     }
-    
-    
+
     /**
      * @return array
      */
@@ -118,9 +120,9 @@ class Pagination
         if ($this->totalPages - $this->page > 0) {
             $result['next'] = $this->page + 1;
         }
-        
+
         if ($this->totalPages - $this->page + $this->length > 3) {
-			$result['preLast'] = $this->totalPages - 1;
+            $result['preLast'] = $this->totalPages - 1;
         }
 
         return $this->uiKeys ? $result : [];
@@ -133,22 +135,22 @@ class Pagination
     {
         $result = [];
         $for = [];
-        
-		if ($this->page - $this->length > 1) {
-			$result[] = 1;
-		}
+
+        if ($this->page - $this->length > 1) {
+            $result[] = 1;
+        }
 
         if ($this->pos !== 'noLeftDots') {
-			if ($this->uiKeys) {
-				$result['leftDots'] = '...';
-			} else {
-				$result[] = '...';
-			}
+            if ($this->uiKeys) {
+                $result['leftDots'] = '...';
+            } else {
+                $result[] = '...';
+            }
         }
-		
-		foreach (range($this->page - 1, $this->page - $this->length) as $value) {
+
+        foreach (range($this->page - 1, $this->page - $this->length) as $value) {
             if ($value > 0) {
-				$for[] = $value;
+                $for[] = $value;
             }
         }
 
@@ -161,39 +163,42 @@ class Pagination
     private function rightPad(): array
     {
         $result = [];
-        
- 		foreach (range($this->page + 1, $this->page + $this->length) as $value) {
-			if ($value <= $this->totalPages) {
-				$result[] = $value;
-		        }
-		}
+
+        foreach (range($this->page + 1, $this->page + $this->length) as $value) {
+            if ($value <= $this->totalPages) {
+                $result[] = $value;
+            }
+        }
 
         if ($this->pos !== 'noRightDots') {
-			if ($this->uiKeys) {
-				$result['rightDots'] = '...';
-			} else {
-				$result[] = '...';
-			}
+            if ($this->uiKeys) {
+                $result['rightDots'] = '...';
+            } else {
+                $result[] = '...';
+            }
         }
-        
+
         if ($this->totalPages - $this->page - $this->length > 0) {
-			$result[] = $this->totalPages;
+            $result[] = $this->totalPages;
         }
 
         return $result;
     }
-    
+
+    /**
+     * @return array
+     */
     private function getCurrent(): array
     {
-		if ($this->uiKeys) {
-				$result['current'] = $this->page;
-			} else {
-				$result[] = $this->page;
-			}
-			
-			return $result;
+        if ($this->uiKeys) {
+            $result['current'] = $this->page;
+        } else {
+            $result[] = $this->page;
+        }
+
+        return $result;
     }
-    
+
     /**
      * @return array
      */
